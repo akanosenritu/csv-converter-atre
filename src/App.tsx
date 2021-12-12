94316ed16f35eef30ecce574788c6f3fd7c899ae
akasen@lenovo-x250 <akasen.org@gmail.com>
2021-12-12 19:07:01 +0900

    modified the script to include placeholders in the place of missing numbers, so that copy and pasting works well

--- src/App.tsx
+++ src/App.tsx
@@ -25,32 +25,26 @@ const convertCSV = async (file: File) => {
     return newDatum
   })
 
-  // split the items into 3: new, old, not-found
-  // and sort them in their categories by item-number
-  const sortNewItems = (items: {[key:string]: string}[]) => {
-    return items.sort((a, b) => {
-      const aItemCode = a["商品番号"]
-      const bItemCode = b["商品番号"]
-      return aItemCode > bItemCode? 1: -1
-    })
+  // generate required item numbers
+  const order1 = Array(31).fill(0).map((elem, index) => "新" + (index+1).toString())
+  const order2 = Array(233).fill(0).map((elem, index) => (index+1).toString())
+  const requiredItemNumbers = order1.concat(order2)
+  
+  // insert placeholders in the place of an item if no data was provided by POS 
+  const tempData: any[] = []
+  for (const num of requiredItemNumbers) {
+    const index = modifiedData.findIndex(datum => datum["商品番号"] === num)
+    if (index === -1) tempData.push({"商品番号": num.toString()})
+    else tempData.push(modifiedData[index])
   }
-  const sortOldItems = (items: {[key:string]: string}[]) => {
-    return items.sort((a, b) => {
-      const aItemCode = parseFloat(a["商品番号"])
-      const bItemCode = parseFloat(b["商品番号"])
-      return aItemCode > bItemCode? 1: -1  
-    })
-  }
-  const newArrivals = sortNewItems(modifiedData.filter(datum => datum["商品番号"].startsWith("新")))
-  const old = sortOldItems(modifiedData
-    .filter(datum => !datum["商品番号"].startsWith("新"))
-    .filter(datum => datum["商品番号"] !== "Not found")
-  )
-  const notFound = modifiedData.filter(datum => datum["商品番号"] === "Not found")
-  console.log(newArrivals.length, old.length, notFound.length)
-  // joint them 
-  const finalData = newArrivals.concat(old).concat(notFound)
 
+  // items that don't have an item-number associated with them
+  const notFound = modifiedData.filter(datum => datum["商品番号"] === "Not found")
+
+  // joint them
+  const finalData = tempData.concat(notFound)
+
+  // stringify the data
   return stringify(finalData, {
     header: true,
     columns: [{key: "商品番号"}, {key: "商品コード"}, {key: "商品名"}, {key: "部門名"}, {key: "純売上"}, {key: "純売上(税抜)"}, {key: "消費税"}, {key: "原価"}, {key: "純売上構成比"}, {key: "販売点数"}, {key: "返品数"}, {key: "販売点数 (net)"}, {key: "販売点数構成比"}, {key: "在庫数"}]
