426c18c7630a3680aed9f9d44f8786500a5135f6
akasen@lenovo-x250 <akasen.org@gmail.com>
2021-12-14 15:37:21 +0900

    updated to include the items that are to be added on 12/16.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import {useState} from "react"
 import {useDropzone} from "react-dropzone"
 import {parse} from "csv-parse/lib/sync"
 import {stringify} from "csv-stringify/lib/sync"
-import janData from "./jan1211.json"
+import janData1211 from "./jan1211.json"
+import janData1216 from "./jan1216.json"
 import iconv from "iconv-lite"
 
 type Datum = {
@@ -47,27 +48,36 @@ const readCSV = async (file: File) => {
   return data as Datum[]
 }
 
-const convertCSV = async (file: File) => {
+const addItemNumber = (datum: Datum, janDataSource: "1211" | "1216") => {
+  const janData = janDataSource === "1211" ? janData1211: janData1216
+  const newDatum = {...datum}
+  newDatum["商品番号"] = "Not found"
+  newDatum["販売点数 (net)"] = "Error" 
+  // @ts-ignore
+  if (janData[datum["商品コード"]]) {
+    // @ts-ignore
+    newDatum["商品番号"] = janData[datum["商品コード"]]
+  }
+  return newDatum
+}
+
+const convertCSV = async (file: File, janData: "1211" | "1216") => {
   const data = await readCSV(file)
 
   // add "Item code" to the data by looking up final.json
   // calculate the actual number of items sold 
   const modifiedData = data.map(datum => {
-    const newDatum = {...datum}
-    newDatum["商品番号"] = "Not found"
-    newDatum["販売点数 (net)"] = "Error" 
-    // @ts-ignore
-    if (janData[datum["商品コード"]]) {
-      // @ts-ignore
-      newDatum["商品番号"] = janData[datum["商品コード"]]
-    }
+    const newDatum = addItemNumber(datum, janData)
     newDatum["販売点数 (net)"] = calculateItemsSold(datum)
     return newDatum
   })
 
   // generate required item numbers
   const order1 = Array(31).fill(0).map((elem, index) => "新" + (index+1).toString())
-  const order2 = Array(233).fill(0).map((elem, index) => (index+1).toString())
+  // before 1216: 233 items
+  // after 1216: 259 items
+  const numberOfOldItems = janData === "1211"? 233: 259
+  const order2 = Array(numberOfOldItems).fill(0).map((elem, index) => (index+1).toString())
   const requiredItemNumbers = order1.concat(order2)
   
   // insert placeholders in the place of an item if no data was provided by POS 
@@ -97,7 +107,11 @@ const stringifyCSV = (data: Datum[]) => {
 
 
 function App() {
+  // due to the addition of items on 12/16, two states are now used.
+  // one is for before 12/1
   const [convertedData, setConvertedData] = useState<Datum[]|null>(null)
+  const [convertedData2, setConvertedData2] = useState<Datum[]|null>(null)
+
   const filterFiles = (acceptedFiles: File[]) => {
     return acceptedFiles.length === 0? null: acceptedFiles[0]
   }
@@ -105,8 +119,11 @@ function App() {
     onDrop: (files) => {
       const file = filterFiles(files)
       if (file) {
-        convertCSV(file)
+        convertCSV(file, "1211")
           .then(data => setConvertedData(data))
+        convertCSV(file, "1216")
+          .then(data => setConvertedData2(data))
+
       }   
     }    
   })
@@ -126,10 +143,21 @@ function App() {
       downloadCSV(stringifyCSV(convertedData))
     }
   }
+  const onClickDownloadButton1216 = async () => {
+    if (convertedData2) {
+      downloadCSV(stringifyCSV(convertedData2))
+    }
+  }
 
   const onClickCopyButton = async () => {
     if (convertedData) {
       const salesData = convertedData.map(datum => calculateItemsSold(datum))
+      navigator.clipboard.writeText(salesData.join("\n"))
+    }
+  }
+  const onClickCopyButton1216 = async () => {
+    if (convertedData2) {
+      const salesData = convertedData2.map(datum => calculateItemsSold(datum))
       navigator.clipboard.writeText(salesData.join("\n"))
     }
   }
@@ -144,14 +172,28 @@ function App() {
           </p>
         </div>
       </div>  
-      <div style={{display: "flex", justifyContent: "space-around"}}>
-        <button onClick={onClickDownloadButton} disabled={!convertedData}>ダウンロード</button>
-        <button onClick={onClickCopyButton} disabled={!convertedData}>クリップボードにコピー</button>
+      <div style={{display: "flex", margin: 5}}>
+        <div style={{width: "50%"}}>235番以降の商品追加前</div>
+        <div style={{display: "flex", justifyContent: "space-around", width: "50%"}}>
+          <button onClick={onClickDownloadButton} disabled={!convertedData}>ダウンロード</button>
+          <button onClick={onClickCopyButton} disabled={!convertedData}>クリップボードにコピー</button>
+        </div>
+      </div>  
+      <div style={{display: "flex", margin: 5}}>
+        <div style={{width: "50%"}}>235番以降の商品追加後</div>
+        <div style={{display: "flex", justifyContent: "space-around", width: "50%"}}>
+          <button onClick={onClickDownloadButton1216} disabled={!convertedData2}>ダウンロード</button>
+          <button onClick={onClickCopyButton1216} disabled={!convertedData2}>クリップボードにコピー</button>
+        </div>
       </div>
-      <div style={{width: "60%", margin: "auto", textAlign:"left"}}>
+      <div style={{width: "90%", margin: "auto", textAlign:"left"}}>
         <ul>
           <li>スマレジからダウンロードするときに"Windows (shift-jis)"を選択したファイルを使用してください</li>
           <li>ダウンロードされたファイルはWindows向けに作成されています。Macで開くと文字化けが起こります。</li>
+          <li>
+            クリップボードにコピーをクリックすると、販売点数の列だけがクリップボードにコピーされます。
+            Excel上にそのまま貼り付けることができます。
+          </li>
         </ul>  
       </div>
     </div>
